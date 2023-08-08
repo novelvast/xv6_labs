d6a25d380160478a378c8e1218202111fa77ecdc
Xinhao_Zhao <2272046066@qq.com>
2023-08-08 21:28:17 +0800

    lab1_2

--- user/primes.c
+++ user/primes.c
@@ -2,45 +2,60 @@
 #include "kernel/stat.h"
 #include "user/user.h"
  
- 
-int main() {
-    int p1[2], i;
-    pipe(p1);
-
-    for (i = 2; i <= 35; i++) {
-        write(p1[1], &i, sizeof(i));
+int main(int argc, char *argv[])
+{
+    int prev[2];
+    pipe(prev);
+    int ret = fork();
+    if (ret < 0)
+    {
+        fprintf(2, "error in fork\n");
     }
-
-    i = 0;
-    write(p1[1], &i, sizeof(i));    // end signal
-
-    while (1) {
-        int n, prime, p2[2];
-        pipe(p2);
-        read(p1[0], &prime, sizeof(prime));
-        printf("prime %d\n", prime);
-        if (read(p1[0], &n, sizeof(n)) && n) {
-            if (fork() == 0) {
-                //子进程会复制一遍父进程的两个管道，其中父进程用来与祖父进程交流的管道应该被关闭
-                close(p1[0]);
-                close(p1[1]);
-                p1[0] = p2[0];
-                p1[1] = p2[1];
-                continue;
+    else if (ret == 0)
+    {
+        int base = 0;
+        while (1)
+        {
+            close(prev[1]); //关闭写端
+            if (read(prev[0], &base, sizeof(base)) == 0)
+            {
+                exit(0);
             }
-            do {
-                if (n % prime != 0)
-                    write(p2[1], &n, sizeof(n));
-            } while (read(p1[0], &n, sizeof(n)) && n);
-            write(p2[1], &n, sizeof(n));
+            printf("prime %d\n", base);
+            int next[2];
+            pipe(next);
+            int r = fork();
+            // 父进程从读端筛选质数
+            if (r > 0)
+            {
+                int cur = 0;
+                close(next[0]);
+                for (; read(prev[0], &cur, sizeof(cur)) != 0;)
+                {
+                    if (cur % base != 0)
+                    {
+                        write(next[1], &cur, sizeof(cur));
+                    }
+                }
+                close(prev[0]);
+                close(next[1]);
+                wait(0);
+                exit(0);
+            }
+            close(prev[0]);
+            prev[0] = next[0];
+            prev[1] = next[1];
         }
-        //关闭占用的管道
-        close(p1[0]);
-        close(p1[1]);
-        close(p2[0]);
-        close(p2[1]);
-        break;
     }
-    wait(0);
+    else
+    {
+        close(prev[0]);
+        for (int i = 2; i <= 35; i++)
+        {
+            write(prev[1], &i, sizeof(int));
+        }
+        close(prev[1]);
+        wait(0);
+    }
     exit(0);
 }
